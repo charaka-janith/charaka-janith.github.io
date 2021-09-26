0678409263569e7e0db7ab4694ba2e7c5463f4ec
dark-sl <charaka.janith.bandarawaththa@gmail.com>
2021-09-26 11:48:18 +0530

    banner completed, going for scrollbar and 3d

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import man_dance from '../assets/gifs/man_dance.gif'
 import Typical from 'react-typical'
 
-export default function Banner() {
+export default function About() {
   return (
     <section id="Banner">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
         </nav>
         <a href="/#"
           className="cursor-pointer text-white inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
+          Contact Me
         </a>
       </div>
     </header>
