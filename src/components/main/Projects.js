4df7338960cc762580ad38a63b29ff192cc86981
dark-sl <charaka.janith.bandarawaththa@gmail.com>
2021-10-31 12:09:10 +0530

    projects and keywords

--- src/components/main/Projects.js
+++ src/components/main/Projects.js
@@ -16,6 +16,8 @@ export default function Projects() {
           {projects.map((project) => (
             <a
               href={project.link}
+              target="_blank"
+              rel="noreferrer"
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
@@ -24,11 +26,11 @@ export default function Projects() {
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-text_dark bg-hover_dark opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-lg font-medium text-white mb-3 font-bold">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
--- src/config/Data.js
+++ src/config/Data.js
@@ -23,28 +23,13 @@ import peraAgros from '../assets/images/projects/pera-agros.png';
 
 export const projects = [
   {
-    title: "Pera-Agros",
-    subtitle: "sample sub",
+    title: "Pera Agros 91/92 FaRMS",
+    subtitle: "Wordpress web-application",
     description:
-      "sample desc",
+      "Scholarship platform application for past pupils association in Peradeniya campus Agriculture faculty",
     image: peraAgros,
     link: "https://peraagros91-92farms.org/",
   }
-];
-
-export const testimonials = [
-  {
-    quote: "I had the pleasure of working with him on an exciting project for over 18 months.We have shared some highs, lows and some real head scratching moments. He has been a real asset to the team and to me personally.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    firstName: "Jesse",
-    lastName: "Hicks"
-  },
-  {
-    quote: "He has been a real asset to the team and able to pick up new technologies quickly. He has a passion for all flavours of development and is a team player with an excellent attitude both in and outside of work.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    firstName: "Ruben",
-    lastName: "Alvarez"
-  },
 ];
 
 export const skills = [
