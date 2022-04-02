85e9882478326f19594cbb13578f508cdc811fca
dark-sl <charaka.janith.bandarawaththa@gmail.com>
2022-04-02 08:29:59 +0530

    header

--- src/components/main/MainNavbar.js
+++ src/components/main/MainNavbar.js
@@ -6,9 +6,9 @@ export default function MainNavbar() {
     <header className="bg-bg_dark bg-opacity-90 sticky top-0 z-20">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <div className="title-font font-medium text-text_dark mb-4 md:mb-0">
-          <div className="ml-3 text-xl cursor-pointer" onClick={() => scroll.scrollToTop()}>
+          <h1 className="ml-3 text-xl cursor-pointer" onClick={() => scroll.scrollToTop()}>
             CHARAKA JANITH
-          </div>
+          </h1>
         </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center text-white cursor-pointer">
           <Link className="inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-gray-800 hover:bg-opacity-40 rounded text-base mt-4 md:mt-0 duration-1000" to="about" smooth={true} duration={1000}>About</Link>
