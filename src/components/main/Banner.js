e9b508f312ec531f7c6abdf07002a68a469c817e
CHARAKA JANITH <charaka.janith.bandarawaththa@gmail.com>
2022-04-17 01:45:36 +0530

    terminal new

--- src/components/main/Banner.js
+++ src/components/main/Banner.js
@@ -10,24 +10,25 @@ export default function Banner() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <AnimationOnScroll animateIn="animate__fadeInLeftBig">
-            <img
-                className="object-cover object-center rounded"
-                alt="man dance"
-                src={terminal}
-            />
+              <img
+                  className="object-cover object-center rounded"
+                  alt="man dance"
+                  src={terminal}
+              />
             </AnimationOnScroll>
           </div>
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div
+              className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center ml-5">
             <AnimationOnScroll animateIn="animate__fadeInRightBig">
-            <h1 className="title-font text-5xl mb-4 font-medium text-white">
-              Hey, I'm CHARAKA.
-              <br className="sm:inline-block" />
-              Wanna know me ? well I love&nbsp;
-              <br className="sm:inline-block"/>
-              <Typical
-                loop={Infinity}
-                wrapper="b"
-                steps={[
+              <h1 className="title-font text-5xl mb-4 font-medium text-white">
+                Hey, I'm CHARAKA.
+                <br className="sm:inline-block"/>
+                Wanna know me ? well I love&nbsp;
+                <br className="sm:inline-block"/>
+                <Typical
+                    loop={Infinity}
+                    wrapper="b"
+                    steps={[
                   'programming', 2000, 'pentesting', 2000, 'planning', 2000, 'martial arts', 2000, 'technologies', 2000, 'social works', 2000, 'algorithems', 2000, 'teamwork', 2000, 'coding', 2000, 'listening', 2000, 'walking', 2000, 'organizing', 2000, 'dogs', 2000, 'cars', 2000, 'teaching', 2000, 'anime', 2000
                 ]}
               />
